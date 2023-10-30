86e343f678388e71e8bba2d95bd4f67fd2c09c5f
Stefan Grobler <39761609+StefanGrobler78@users.noreply.github.com>
2023-10-30 14:22:15 +0200

    Sticky Top

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,7 @@
 */
 //
 // Scripts
-// 
-
-// const navLinks = document.querySelectorAll('.nav-item')
-// const menuToggle = document.getElementById('navbarText')
-// const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
-// navLinks.forEach((l) => {
-//     l.addEventListener('click', () => { bsCollapse.toggle() })
-// })
+//
 
 const date = document.getElementById('date');
     date.innerHTML = new Date().getFullYear();
@@ -28,3 +21,30 @@ navLinks.forEach((l) => {
             }
     })
 })
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarText .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
